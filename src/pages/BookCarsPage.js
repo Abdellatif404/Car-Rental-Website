fa30f4bbf473933361136cd930cac4da35291cbe
admin test <elbouziadyabdelatif@gmail.com>
2023-01-27 18:53:39 +0100

    styling book cars page

--- src/pages/BookCarsPage.js
+++ src/pages/BookCarsPage.js
@@ -57,45 +57,179 @@ function BookCars() {
         }
       />
 
-      
-<div className="wrapper m-5">
-<div className="vehicle-card">
-		<div className="details">      
-      <div className="thumb-gallery">
-        <img className="first" src="https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/wp-content/uploads/2017/03/2018-Bugatti-Chiron-119.jpg?crop=1xw:1xh;center,center&resize=480:*" />
-        <img className="second" src="https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/wp-content/uploads/2017/03/2018-Bugatti-Chiron-117.jpg?crop=1xw:1xh;center,center&resize=480:*" />        
-      </div>
-			<div className="info">
-    			<h3>Bugatti Chiron</h3>
-				
-				<div className="price">
-					<span>Price Starting at</span>
-					<h4>$2,998,000</h4>
-				</div>
-				<div className="ctas">
-					<a href="#" className="btn-book primary">Model Details</a>
-					<a href="#" className="btn-book secondary">Cars for Sale</a>
-			        <div style={{clear:'both'}}></div>
-		        </div>
-				<div className="specs">
-					<div className="spec mpg">
-						<span>EPA City/Hwy</span>
-						<p>9/14<span/>mpg</p>
-					</div>
-					<div className="spec mpg">
-						<span>0-60</span>
-						<p>2.3<span/>sec</p>
-					</div>
-					<div className="spec mpg">
-						<span>Horsepower</span>
-						<p>1500<span/>hp</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+      <div className="wrapper">
+        <div className="vehicle-card">
+          <div className="details">
+            <div className="thumb-gallery">
+              <img
+                className="first"
+                src="https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/wp-content/uploads/2017/03/2018-Bugatti-Chiron-119.jpg?crop=1xw:1xh;center,center&resize=480:*"
+              />
+              <img
+                className="second"
+                src="https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/wp-content/uploads/2017/03/2018-Bugatti-Chiron-117.jpg?crop=1xw:1xh;center,center&resize=480:*"
+              />
+            </div>
+            <div className="info">
+              <h3>Bugatti Chiron</h3>
 
+              <div className="price">
+                <span>Price Starting at</span>
+                <h4>$2,998,000</h4>
+              </div>
+              <div className="ctas">
+                <a href="#" className="btn-book primary">
+                  Model Details
+                </a>
+                <a href="#" className="btn-book secondary">
+                  Cars for Sale
+                </a>
+                <div style={{ clear: "both" }}></div>
+              </div>
+              <div className="specs">
+                <div className="spec mpg">
+                  <span>EPA City/Hwy</span>
+                  <p>
+                    9/14
+                    <span />
+                    mpg
+                  </p>
+                </div>
+                <div className="spec mpg">
+                  <span>0-60</span>
+                  <p>
+                    2.3
+                    <span />
+                    sec
+                  </p>
+                </div>
+                <div className="spec mpg">
+                  <span>Horsepower</span>
+                  <p>
+                    1500
+                    <span />
+                    hp
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="vehicle-card">
+          <div className="details">
+            <div className="thumb-gallery">
+              <img
+                className="first"
+                src="https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/wp-content/uploads/2017/06/2018-Ferrari-812-Superfast-115.jpg?crop=1xw:1xh;center,center&resize=480:*"
+              />
+              <img
+                className="second"
+                src="https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/wp-content/uploads/2017/06/2018-Ferrari-812-Superfast-101.jpg?crop=1xw:1xh;center,center&resize=480:*"
+              />
+            </div>
+            <div className="info">
+              <h3>Bugatti Chiron</h3>
+
+              <div className="price">
+                <span>Price Starting at</span>
+                <h4>$2,998,000</h4>
+              </div>
+              <div className="ctas">
+                <a href="#" className="btn-book primary">
+                  Model Details
+                </a>
+                <a href="#" className="btn-book secondary">
+                  Cars for Sale
+                </a>
+                <div style={{ clear: "both" }}></div>
+              </div>
+              <div className="specs">
+                <div className="spec mpg">
+                  <span>EPA City/Hwy</span>
+                  <p>
+                    9/14
+                    <span />
+                    mpg
+                  </p>
+                </div>
+                <div className="spec mpg">
+                  <span>0-60</span>
+                  <p>
+                    2.3
+                    <span />
+                    sec
+                  </p>
+                </div>
+                <div className="spec mpg">
+                  <span>Horsepower</span>
+                  <p>
+                    1500
+                    <span />
+                    hp
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="vehicle-card">
+          <div className="details">
+            <div className="thumb-gallery">
+              <img
+                className="first"
+                src="https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/wp-content/uploads/2017/09/2018-Mercedes-AMG-GT-GTC-116.jpg?crop=1xw:1xh;center,center&resize=480:*"
+              />
+              <img
+                className="second"
+                src="https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/wp-content/uploads/2017/09/2018-Mercedes-AMG-GT-GTC-114.jpg?crop=1xw:1xh;center,center&resize=480:*"
+              />
+            </div>
+            <div className="info">
+              <h3>Bugatti Chiron</h3>
+
+              <div className="price">
+                <span>Price Starting at</span>
+                <h4>$2,998,000</h4>
+              </div>
+              <div className="ctas">
+                <a href="#" className="btn-book primary">
+                  Model Details
+                </a>
+                <a href="#" className="btn-book secondary">
+                  Cars for Sale
+                </a>
+                <div style={{ clear: "both" }}></div>
+              </div>
+              <div className="specs">
+                <div className="spec mpg">
+                  <span>EPA City/Hwy</span>
+                  <p>
+                    9/14
+                    <span />
+                    mpg
+                  </p>
+                </div>
+                <div className="spec mpg">
+                  <span>0-60</span>
+                  <p>
+                    2.3
+                    <span />
+                    sec
+                  </p>
+                </div>
+                <div className="spec mpg">
+                  <span>Horsepower</span>
+                  <p>
+                    1500
+                    <span />
+                    hp
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
